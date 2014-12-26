d67fec77d1fa4e43b0514ba20de9bb2211a2d97f
Aleric Inglewood <Aleric.Inglewood@gmail.com>
2014-12-26 18:41:57 +0100

    Move debug ostream operator<< for AIAlert::Error to utils.

--- src/cwd/debug_ostream_operators.h
+++ src/cwd/debug_ostream_operators.h
@@ -1,7 +1,24 @@
-// speech -- Speech recognition
-//
-//! @file debug_ostream_operators.h
-//! @brief This file contains the declaration of debug serializers.
+/**
+ * speech -- Jack client for speech recognition.
+ *
+ * \file debug_ostream_operators.h
+ * \brief This file contains the declaration of debug serializers.
+ *
+ * Copyright (C) 2014 Aleric Inglewood.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef DEBUG_OSTREAM_OPERATORS_H
 #define DEBUG_OSTREAM_OPERATORS_H
@@ -34,12 +51,6 @@ inline char const* type_name_of(void)
 }
 
 } // namespace debug
-
-namespace AIAlert {
-  class Error;
-} // namespace AIAlert
-
-extern std::ostream& operator<<(std::ostream& os, AIAlert::Error const& error);
 
 struct timeval;
 
